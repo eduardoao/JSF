3ba56ea4e8fbff8be9e7811bec8d5dc9c6eb4a3d
Eduardo <eduardo.oliveira@trt4.jus.br>
2021-05-07 12:55:30 -0300

    fix(alura-jsf):atualizacao cdi e primefaces

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,17 +1,43 @@
 package br.com.caelum.livraria.bean;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Livro;
+import br.com.caelum.livraria.modelo.Autor;
 
+@SuppressWarnings("deprecation")
 @ManagedBean
+@ViewScoped
 public class LivroBean {
 
 	private Livro livro = new Livro();
+	private Integer autorId;
+
+	public Integer getAutorId() {
+		return autorId;
+	}
+
+	public void setAutorId(Integer autorId) {
+		this.autorId = autorId;
+	}
+
+	private List<Autor> autores;
+
+	public List<Autor> getAutores() {
+		autores = new DAO<Autor>(Autor.class).listaTodos();
+		return autores;
+	}
 
 	public Livro getLivro() {
 		return livro;
+	}
+
+	public List<Autor> getAutoresDoLivroAtual() {
+		return this.livro.getAutores();
 	}
 
 	public void gravar() {
@@ -21,7 +47,13 @@ public class LivroBean {
 			throw new RuntimeException("Livro deve ter pelo menos um Autor.");
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		new DAO<Livro>(Livro.class).adiciona(this.livro);		
+		this.livro.getAutores().clear();
+	}
+
+	public void gravarAutor() {
+		Autor autorSelecionado = new DAO<Autor>(Autor.class).buscaPorId(this.autorId);
+		this.livro.adicionaAutor(autorSelecionado);
 	}
 
 }
