3bf6a0c48db4aecfa4801fdf520470a43f62ee62
Eduardo <eduardo.oliveira@trt4.jus.br>
2021-05-10 16:36:04 -0300

    feat:(alura-jsf):inclusao da funcionalidade alterar

--- src/br/com/caelum/livraria/bean/LivroBean.java
+++ src/br/com/caelum/livraria/bean/LivroBean.java
@@ -1,6 +1,7 @@
 package br.com.caelum.livraria.bean;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -9,6 +10,8 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.component.UIComponent;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
+import javax.persistence.FetchType;
+import javax.persistence.ManyToMany;
 
 import br.com.caelum.livraria.dao.DAO;
 import br.com.caelum.livraria.modelo.Livro;
@@ -22,11 +25,13 @@ public class LivroBean implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Livro livro = new Livro();
-	
+
 	private List<Livro> livros;
 
-
 	private Integer autorId;
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Autor> autores = new ArrayList<Autor>();
 
 	public void setAutorId(Integer autorId) {
 		this.autorId = autorId;
@@ -41,12 +46,13 @@ public class LivroBean implements Serializable {
 	}
 
 	public List<Livro> getLivros() {
-		livros =  new DAO<Livro>(Livro.class).listaTodos();
+		livros = new DAO<Livro>(Livro.class).listaTodos();
 		return livros;
 	}
 
 	public List<Autor> getAutores() {
-		return new DAO<Autor>(Autor.class).listaTodos();
+		autores = new DAO<Autor>(Autor.class).listaTodos();
+		return autores;
 	}
 
 	public List<Autor> getAutoresDoLivro() {
@@ -68,7 +74,11 @@ public class LivroBean implements Serializable {
 			return;
 		}
 
-		new DAO<Livro>(Livro.class).adiciona(this.livro);
+		if (this.livro.getId() == null) {
+			new DAO<Livro>(Livro.class).adiciona(livro);
+		} else {
+			new DAO<Livro>(Livro.class).atualiza(this.livro);
+		}
 
 		this.livro = new Livro();
 	}
@@ -86,12 +96,16 @@ public class LivroBean implements Serializable {
 		System.out.println("Chamando o formulário do Autor");
 		return "autor?faces-redirect=true";
 	}
-	
-	
+
 	public void remover(Livro livro) {
-	    System.out.println("Removendo livro " + livro.getTitulo());
-	    new DAO<Livro>(Livro.class).remove(livro);
-	    this.livros.remove(livro);
+		System.out.println("Removendo livro " + livro.getTitulo());
+		new DAO<Livro>(Livro.class).remove(livro);
+		this.livros.remove(livro);
 	}
-	
+
+	public void carregar(Livro livro) {
+		System.out.println("Carregando o livro: " + livro.getTitulo());
+		this.livro = livro;
+	}
+
 }
